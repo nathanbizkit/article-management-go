d31f850bbded0407eaeaae244677fc6ea0f814c0
Nuttawut Malee <nuttawutmalee@gmail.com>
2024-09-28 14:36:26 +0700

    Fix broken sql query in db/script

--- backend/db/script/99_seeding.sql
+++ backend/db/script/99_seeding.sql
@@ -2,10 +2,10 @@ INSERT INTO
     article_management.users (username, email, password, name, bio, image)
 VALUES
     (
-        "admin",
-        "admin@mail.com",
-        "$2a$10$eHi1PdtgD4vrBZuB53uRRuBM8nrxMpupBAf3iT7zRnDhPq2vxINfi", --password: password
-        "ADMIN",
-        "I am an admin.",
-        "https://picsum.photos/id/22/300/300"
+        'admin',
+        'admin@mail.com',
+        '$2a$10$eHi1PdtgD4vrBZuB53uRRuBM8nrxMpupBAf3iT7zRnDhPq2vxINfi', --password: password
+        'ADMIN',
+        'I am an admin.',
+        'https://picsum.photos/id/22/300/300'
     );
