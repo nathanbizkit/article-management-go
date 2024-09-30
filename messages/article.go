36f4f69172a965eaacc37675d5ffc1190ef9614f
Nuttawut Malee <nuttawutmalee@gmail.com>
2024-09-30 14:01:32 +0700

    Add messages for http handlers

---
+++ messages/article.go
@@ -0,0 +1,66 @@
+package messages
+
+import "time"
+
+/* Request message */
+
+// CreateArticleRequest definition
+type CreateArticleRequest struct {
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Body        string   `json:"body"`
+	Tags        []string `json:"tags"`
+}
+
+// UpdateArticleRequest definition
+type UpdateArticleRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Body        string `json:"body"`
+}
+
+// CreateCommentRequest definition
+type CreateCommentRequest struct {
+	Body string `json:"body"`
+}
+
+/* Response message */
+
+// ArticleResponse definition
+type ArticleResponse struct {
+	ID             uint            `json:"id"`
+	Title          string          `json:"title"`
+	Description    string          `json:"description"`
+	Body           string          `json:"body"`
+	Tags           []string        `json:"tags"`
+	Favorited      bool            `json:"favorited"`
+	FavoritesCount int64           `json:"favorites_count"`
+	Author         ProfileResponse `json:"author"`
+	CreatedAt      time.Time       `json:"created_at"`
+	UpdatedAt      *time.Time      `json:"updated_at"`
+}
+
+// ArticlesResponse definition
+type ArticlesResponse struct {
+	Articles      []ArticleResponse `json:"articles"`
+	ArticlesCount int64             `json:"articles_count"`
+}
+
+// TagsResponse definition
+type TagsResponse struct {
+	Tags []string `json:"tags"`
+}
+
+// CommentResponse definition
+type CommentResponse struct {
+	ID        uint              `json:"id"`
+	Body      string            `json:"body"`
+	Author    []ProfileResponse `json:"author"`
+	CreatedAt time.Time         `json:"created_at"`
+	UpdatedAt *time.Time        `json:"updated_at"`
+}
+
+// CommentsResponse definition
+type CommentsResponse struct {
+	Comments []CommentResponse `json:"comments"`
+}
---
+++ messages/user.go
@@ -0,0 +1,53 @@
+package messages
+
+import "time"
+
+/* Request message */
+
+// LoginUserRequest definition
+type LoginUserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// CreateUserRequest definition
+type CreateUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+// UpdateUserRequest definition
+type UpdateUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+	Bio      string `json:"bio"`
+	Image    string `json:"image"`
+}
+
+/* Response message */
+
+// UserReponse definition
+type UserReponse struct {
+	ID               uint              `json:"id"`
+	Username         string            `json:"username"`
+	Email            string            `json:"email"`
+	Name             string            `json:"name"`
+	Bio              string            `json:"bio"`
+	Image            string            `json:"image"`
+	Follows          []ProfileResponse `json:"follows"`
+	FavoriteArticles []ArticleResponse `json:"articles"`
+	CreatedAt        time.Time         `json:"created_at"`
+	UpdatedAt        *time.Time        `json:"updated_at"`
+}
+
+// ProfileResponse definition
+type ProfileResponse struct {
+	Username  string `json:"username"`
+	Bio       string `json:"bio"`
+	Image     string `json:"image"`
+	Following bool   `json:"following"`
+}
