98177efb8fc49505b49baebd58458bee698f560f
Nuttawut Malee <nuttawutmalee@gmail.com>
2024-11-14 09:50:08 +0700

    Add ssl public cert to e2e test script

--- test/e2e/run-api-tests.sh
+++ test/e2e/run-api-tests.sh
@@ -3,13 +3,14 @@ set -x
 
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-APIURL=${APIURL:-http://localhost:8000}
+APIURL=${APIURL:-https://localhost:8000}
 USERNAME=${USERNAME:-u$(date +%s)}
 EMAIL=${EMAIL:-$USERNAME@mail.com}
-PASSWORD=${PASSWORD:-password}
+PASSWORD=${PASSWORD:-pA55w0Rd!}
 
 npx newman run $SCRIPTDIR/Conduit.postman_collection.json \
     --delay-request 500 \
+    --ssl-client-cert ~/certs/localCA.pem \
     --global-var "APIURL=$APIURL" \
     --global-var "USERNAME=$USERNAME" \
     --global-var "EMAIL=$EMAIL" \
